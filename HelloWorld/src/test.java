7770670d7adcddb7d6272a6102c9f689c55ca143
JKtim <896934718@qq.com>
2019-04-09 17:30:02 +0800

    branch dev change

--- HelloWorld/src/test.java
+++ HelloWorld/src/test.java
@@ -2,6 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("this is your test");
         System.out.println("go and fuck yourself");
-        System.out.println("hello world");
+        System.out.println("fuck fuck fuck");
     }
 }
